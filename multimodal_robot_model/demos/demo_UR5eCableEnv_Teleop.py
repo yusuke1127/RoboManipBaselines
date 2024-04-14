e2c0f139e6ea07464db50a514a884595702589e1
Masaki Murooka <m-murooka@aist.go.jp>
2024-04-14 22:06:48 +0900

    Refactor demo_UR5eCableEnv_Teleop.

--- multimodal_robot_model/demos/demo_UR5eCableEnv_Teleop.py
+++ multimodal_robot_model/demos/demo_UR5eCableEnv_Teleop.py
@@ -1,4 +1,3 @@
-import os
 import numpy as np
 import cv2
 import gymnasium as gym
@@ -6,54 +5,7 @@ import multimodal_robot_model
 import pinocchio as pin
 import pyspacemouse
 import mujoco
-
-def reset_recording(pole_pos_idx=None):
-    global status, data_seq
-
-    # Set position of poles
-    pole_pos_offsets = np.array([
-        [-0.03, 0, 0.0],
-        [0.0, 0, 0.0],
-        [0.03, 0, 0.0],
-        [0.06, 0, 0.0],
-        [0.09, 0, 0.0],
-        [0.12, 0, 0.0],
-    ]) # [m]
-    if pole_pos_idx is None:
-        pole_pos_idx = data_idx % len(pole_pos_offsets)
-    env.unwrapped.model.body("poles").pos = original_pole_pos + pole_pos_offsets[pole_pos_idx]
-
-    # Set environment index (currently only the position of poles is a dependent parameter)
-    env_idx = pole_pos_idx
-
-    # Reset variables for recording
-    status = "initial"
-    data_seq = {
-        "time": [],
-        "joint": [],
-        "front_image": [],
-        "side_image": [],
-        "wrench": [],
-    }
-    print("== [UR5eCableEnv] data_idx: {}, env_idx: {} ==".format(data_idx, env_idx))
-    print("- Press space key to start automatic grasping.")
-
-    return env_idx
-
-def get_status_image(status):
-    status_image = np.zeros((50, 224, 3), dtype=np.uint8)
-    if status == "initial":
-        status_image[:, :] = np.array([200, 255, 200])
-    elif status == "pre-reach" or status == "reach" or status == "grasp":
-        status_image[:, :] = np.array([255, 255, 200])
-    elif status == "record":
-        status_image[:, :] = np.array([255, 200, 200])
-    elif status == "end":
-        status_image[:, :] = np.array([200, 200, 255])
-    else:
-        raise ValueError("Unknown status: {}".format(status))
-    cv2.putText(status_image, status, (5, 35), cv2.FONT_HERSHEY_DUPLEX, 1.0, (0, 0, 0), 2)
-    return status_image
+from utils_UR5eCableEnv import MotionManager, RecordStatus, RecordKey, RecordManager
 
 # Setup gym
 env = gym.make(
@@ -62,161 +14,114 @@ env = gym.make(
   extra_camera_configs=[{"name": "front", "size": (224, 224)}, {"name": "side", "size": (224, 224)}]
 )
 obs, info = env.reset(seed=42)
-action = np.zeros(env.action_space.shape)
 
-# Setup pinocchio
-root_se3 = pin.SE3(np.identity(3), np.array([-0.605, 0.0, 0.8])) # env_ur5e_cable_verticalup.xml
-# root_se3 = pin.SE3(np.matmul(pin.rpy.rpyToMatrix(2.35619, 0.0, 0.0), pin.rpy.rpyToMatrix(0.0, 0.0, -1.5708)), np.array([-0.27, -0.18, 1.32])) # env_ur5e_cable_diagonaldown.xml
-model = pin.buildModelFromUrdf(env.unwrapped.urdf_path)
-model.jointPlacements[1] = root_se3.act(model.jointPlacements[1])
-data = model.createData()
-joint_pos = env.unwrapped.init_qpos[:6].copy()
-eef_joint_id = 6
-pin.forwardKinematics(model, data, joint_pos)
-original_target_se3 = data.oMi[eef_joint_id].copy()
-target_se3 = original_target_se3.copy()
-gripper_pos = 0.0
+# Setup motion manager
+motion_manager = MotionManager(env)
+
+# Setup record manager
+record_manager = RecordManager(env)
 
 # Setup spacemouse
 pyspacemouse.open()
 
-# Setup data recording
-data_idx = 0
-status_list = ["initial", "pre-reach", "reach", "grasp", "record", "end"]
-original_pole_pos = env.unwrapped.model.body("poles").pos.copy()
-env_idx = reset_recording()
-
+reset = True
 while True:
-    # Solve FK
-    pin.forwardKinematics(model, data, joint_pos)
-    current_se3 = data.oMi[eef_joint_id]
+    # Reset
+    if reset:
+        motion_manager.reset()
+        record_manager.reset()
+        record_manager.setupSimWorld()
+        obs, info = env.reset()
+        print("== [UR5eCableEnv] data_idx: {}, world_idx: {} ==".format(record_manager.data_idx, record_manager.world_idx))
+        print("- Press space key to start automatic grasping.")
+        reset = False
 
     # Read spacemouse
     spacemouse_state = pyspacemouse.read()
 
     # Set arm command
-    if status == "pre-reach":
+    if record_manager.status == RecordStatus.PRE_REACH:
         target_pos = env.unwrapped.model.body("cable_end").pos.copy()
         target_pos[2] = 1.02 # [m]
-        target_se3 = pin.SE3(np.diag([-1.0, 1.0, -1.0]), target_pos)
-    elif status == "reach":
+        motion_manager.target_se3 = pin.SE3(np.diag([-1.0, 1.0, -1.0]), target_pos)
+    elif record_manager.status == RecordStatus.REACH:
         target_pos = env.unwrapped.model.body("cable_end").pos.copy()
         target_pos[2] = 0.995 # [m]
-        target_se3 = pin.SE3(np.diag([-1.0, 1.0, -1.0]), target_pos)
-    elif status == "record":
+        motion_manager.target_se3 = pin.SE3(np.diag([-1.0, 1.0, -1.0]), target_pos)
+    elif record_manager.status == RecordStatus.TELEOP:
         pos_scale = 1e-2
-        target_se3.translation += pos_scale * np.array([-1.0 * spacemouse_state.y, spacemouse_state.x, spacemouse_state.z])
-        ori_scale = 5e-3
-        rpy = ori_scale * np.array([-1.0 * spacemouse_state.roll, -1.0 * spacemouse_state.pitch, -2.0 * spacemouse_state.yaw])
-        target_se3.rotation = np.matmul(pin.rpy.rpyToMatrix(*rpy), target_se3.rotation)
+        delta_pos = pos_scale * np.array([-1.0 * spacemouse_state.y, spacemouse_state.x, spacemouse_state.z])
+        rpy_scale = 5e-3
+        delta_rpy = rpy_scale * np.array([-1.0 * spacemouse_state.roll, -1.0 * spacemouse_state.pitch, -2.0 * spacemouse_state.yaw])
+        motion_manager.setRelativeTargetSE3(delta_pos, delta_rpy)
 
     # Set gripper command
-    if status == "grasp":
-        gripper_pos = env.action_space.high[6]
-    elif status == "record":
+    if record_manager.status == RecordStatus.GRASP:
+        motion_manager.gripper_pos = env.action_space.high[6]
+    elif record_manager.status == RecordStatus.TELEOP:
         gripper_scale = 5.0
         if spacemouse_state.buttons[0] > 0 and spacemouse_state.buttons[1] <= 0:
-            gripper_pos = np.clip(gripper_pos + gripper_scale, env.action_space.low[6], env.action_space.high[6])
+            motion_manager.gripper_pos += gripper_scale
         elif spacemouse_state.buttons[1] > 0 and spacemouse_state.buttons[0] <= 0:
-            gripper_pos = np.clip(gripper_pos - gripper_scale, env.action_space.low[6], env.action_space.high[6])
+            motion_manager.gripper_pos -= gripper_scale
 
     # Draw markers
-    env.unwrapped.mujoco_renderer.viewer.add_marker(
-        pos=target_se3.translation,
-        mat=target_se3.rotation,
-        label="",
-        type=mujoco.mjtGeom.mjGEOM_BOX,
-        size=(0.02, 0.02, 0.03),
-        rgba=(0, 1, 0, 0.5))
-    env.unwrapped.mujoco_renderer.viewer.add_marker(
-        pos=current_se3.translation,
-        mat=current_se3.rotation,
-        label="",
-        type=mujoco.mjtGeom.mjGEOM_BOX,
-        size=(0.02, 0.02, 0.03),
-        rgba=(1, 0, 0, 0.5))
+    motion_manager.drawMarkers()
 
     # Solve IK
-    # https://gepettoweb.laas.fr/doc/stack-of-tasks/pinocchio/master/doxygen-html/md_doc_b-examples_d-inverse-kinematics.html
-    error_se3 = current_se3.actInv(target_se3)
-    error_vec = pin.log(error_se3).vector # in joint frame
-    J = pin.computeJointJacobian(model, data, joint_pos, eef_joint_id) # in joint frame
-    J = -1 * np.dot(pin.Jlog6(error_se3.inverse()), J)
-    damping_scale = 1e-6
-    delta_joint_pos = -1 * J.T.dot(np.linalg.solve(J.dot(J.T) + (np.dot(error_vec, error_vec) + damping_scale) * np.identity(6), error_vec))
-    joint_pos = pin.integrate(model, joint_pos, delta_joint_pos)
+    motion_manager.inverseKinematics()
 
     # Step environment
-    action[:6] = joint_pos
-    action[6] = gripper_pos
+    action = motion_manager.getAction()
     obs, _, _, _, info = env.step(action)
 
     # Record data
-    if status == "record":
-        data_seq["time"].append(env.unwrapped.data.time - start_time)
-        data_seq["joint"].append(action)
-        data_seq["front_image"].append(info["images"]["front"])
-        data_seq["side_image"].append(info["images"]["side"])
-        data_seq["wrench"].append(obs[16:])
+    if record_manager.status == RecordStatus.TELEOP:
+        record_manager.appendSingleData(RecordKey.TIME, record_manager.status_elapsed_duration)
+        record_manager.appendSingleData(RecordKey.JOINT, action)
+        record_manager.appendSingleData(RecordKey.FRONT_IMAGE, info["images"]["front"])
+        record_manager.appendSingleData(RecordKey.SIDE_IMAGE, info["images"]["side"])
+        record_manager.appendSingleData(RecordKey.WRENCH, obs[16:])
 
     # Draw images
-    window_image = np.concatenate([info["images"]["front"], info["images"]["side"], get_status_image(status)])
+    window_image = np.concatenate([info["images"]["front"], info["images"]["side"], record_manager.getStatusImage()])
     cv2.imshow("image", cv2.cvtColor(window_image, cv2.COLOR_RGB2BGR))
     key = cv2.waitKey(1)
 
     # Manage status
-    reset = False
-    if status == "initial":
+    if record_manager.status == RecordStatus.INITIAL:
         if key == 32: # space key
-            status = "pre-reach"
-            start_time = env.unwrapped.data.time
-    elif status == "pre-reach":
+            record_manager.goToNextStatus()
+    elif record_manager.status == RecordStatus.PRE_REACH:
         pre_reach_duration = 0.7 # [s]
-        if env.unwrapped.data.time - start_time > pre_reach_duration:
-            status = "reach"
-            start_time = env.unwrapped.data.time
-    elif status == "reach":
+        if record_manager.status_elapsed_duration > pre_reach_duration:
+            record_manager.goToNextStatus()
+    elif record_manager.status == RecordStatus.REACH:
         reach_duration = 0.3 # [s]
-        if env.unwrapped.data.time - start_time > reach_duration:
-            status = "grasp"
-            start_time = env.unwrapped.data.time
+        if record_manager.status_elapsed_duration > reach_duration:
+            record_manager.goToNextStatus()
             print("- Press space key to start teleoperation after robot grasps the cable.")
-    elif status == "grasp":
+    elif record_manager.status == RecordStatus.GRASP:
         if key == 32: # space key
-            status = "record"
-            start_time = env.unwrapped.data.time
+            record_manager.goToNextStatus()
             print("- Press space key to finish teleoperation.")
-    elif status == "record":
+    elif record_manager.status == RecordStatus.TELEOP:
         if key == 32: # space key
-            status = "end"
-            print("- Press the 's' key if the teleoperation succeeded, or the 'f' key if it failed. (duration: {:.1f} [s])".format(data_seq["time"][-1]))
-    elif status == "end":
+            print("- Press the 's' key if the teleoperation succeeded, or the 'f' key if it failed. (duration: {:.1f} [s])".format(
+                record_manager.status_elapsed_duration))
+            record_manager.goToNextStatus()
+    elif record_manager.status == RecordStatus.END:
         if key == ord("s"):
             # Save data
-            dirname = "teleop_data/env{:0>1}".format(env_idx)
-            os.makedirs(dirname, exist_ok=True)
-            filename = "{}/UR5eCableEnv_env{:0>1}_{:0>3}.npz".format(dirname, env_idx, data_idx)
+            filename = "teleop_data/env{:0>1}/UR5eCableEnv_env{:0>1}_{:0>3}.npz".format(
+                record_manager.world_idx, record_manager.world_idx, record_manager.data_idx)
+            record_manager.saveData(filename)
             print("- Teleoperation succeeded: Save the data as {}".format(filename))
-            np.savez(filename,
-                     times=data_seq["time"],
-                     joints=data_seq["joint"],
-                     front_image=data_seq["front_image"],
-                     side_image=data_seq["side_image"],
-                     wrench=data_seq["wrench"])
-            data_idx += 1
             reset = True
         elif key == ord("f"):
             print("- Teleoperation failed: Reset without saving")
             reset = True
     if key == 27: # escape key
         break
-
-    # Check end conditions
-    if reset:
-        env_idx = reset_recording()
-        obs, info = env.reset()
-        joint_pos = env.unwrapped.init_qpos[:6].copy()
-        target_se3 = original_target_se3.copy()
-        gripper_pos = 0.0
 
 # env.close()
---
+++ multimodal_robot_model/demos/utils_UR5eCableEnv.py
@@ -0,0 +1,208 @@
+import os
+import numpy as np
+import cv2
+from enum import Enum
+import gymnasium as gym
+import pinocchio as pin
+import mujoco
+
+class MotionManager(object):
+    """
+    Motion manager for robot arm and gripper.
+
+    The manager computes forward and inverse kinematics using Pinocchio, a robot modeling library.
+    """
+    def __init__(self, env):
+        self.env = env
+
+        # Setup pinocchio model and data
+        root_se3 = pin.SE3(np.identity(3), np.array([-0.605, 0.0, 0.8])) # env_ur5e_cable_verticalup.xml
+        # root_se3 = pin.SE3(np.matmul(pin.rpy.rpyToMatrix(2.35619, 0.0, 0.0), pin.rpy.rpyToMatrix(0.0, 0.0, -1.5708)), np.array([-0.27, -0.18, 1.32])) # env_ur5e_cable_diagonaldown.xml
+        self.pin_model = pin.buildModelFromUrdf(self.env.unwrapped.urdf_path)
+        self.pin_model.jointPlacements[1] = root_se3.act(self.pin_model.jointPlacements[1]) # Set root link pose
+        self.pin_data = self.pin_model.createData()
+
+        # Setup arm
+        self.joint_pos = self.env.unwrapped.init_qpos[:6].copy()
+        self.eef_joint_id = 6
+        pin.forwardKinematics(self.pin_model, self.pin_data, self.joint_pos)
+        self._original_target_se3 = self.pin_data.oMi[self.eef_joint_id].copy()
+        self.target_se3 = self._original_target_se3.copy()
+
+        # Setup gripper
+        self._gripper_pos = 0.0
+        self.gripper_action_idx = 6
+
+    def reset(self):
+        """Reset states of arm and gripper."""
+        self.joint_pos = self.env.unwrapped.init_qpos[:6].copy()
+        self.target_se3 = self._original_target_se3.copy()
+        self.gripper_pos = 0.0
+
+    def inverseKinematics(self):
+        """Solve inverse kinematics."""
+        # https://gepettoweb.laas.fr/doc/stack-of-tasks/pinocchio/master/doxygen-html/md_doc_b-examples_d-inverse-kinematics.html
+        error_se3 = self.current_se3.actInv(self.target_se3)
+        error_vec = pin.log(error_se3).vector # in joint frame
+        J = pin.computeJointJacobian(self.pin_model, self.pin_data, self.joint_pos, self.eef_joint_id) # in joint frame
+        J = -1 * np.dot(pin.Jlog6(error_se3.inverse()), J)
+        damping_scale = 1e-6
+        delta_joint_pos = -1 * J.T.dot(np.linalg.solve(J.dot(J.T) + (np.dot(error_vec, error_vec) + damping_scale) * np.identity(6), error_vec))
+        self.joint_pos = pin.integrate(self.pin_model, self.joint_pos, delta_joint_pos)
+        pin.forwardKinematics(self.pin_model, self.pin_data, self.joint_pos)
+
+    def setRelativeTargetSE3(self, delta_pos=None, delta_rpy=None):
+        """Set the target pose of the end-effector relatively."""
+        if delta_pos is not None:
+            self.target_se3.translation += delta_pos
+        if delta_rpy is not None:
+            self.target_se3.rotation = np.matmul(pin.rpy.rpyToMatrix(*delta_rpy), self.target_se3.rotation)
+
+    def drawMarkers(self):
+        """Draw markers of the current and target poses of the end-effector to viewer."""
+        self.env.unwrapped.mujoco_renderer.viewer.add_marker(
+            pos=self.target_se3.translation,
+            mat=self.target_se3.rotation,
+            label="",
+            type=mujoco.mjtGeom.mjGEOM_BOX,
+            size=(0.02, 0.02, 0.03),
+            rgba=(0, 1, 0, 0.5))
+        self.env.unwrapped.mujoco_renderer.viewer.add_marker(
+            pos=self.current_se3.translation,
+            mat=self.current_se3.rotation,
+            label="",
+            type=mujoco.mjtGeom.mjGEOM_BOX,
+            size=(0.02, 0.02, 0.03),
+            rgba=(1, 0, 0, 0.5))
+
+    def getAction(self):
+        """Get action for Gym."""
+        return np.concatenate([self.joint_pos, [self.gripper_pos]])
+
+    @property
+    def current_se3(self):
+        """Get the current pose of the end-effector."""
+        return self.pin_data.oMi[self.eef_joint_id]
+
+    @property
+    def gripper_pos(self):
+        """Get the target gripper position."""
+        return self._gripper_pos
+
+    @gripper_pos.setter
+    def gripper_pos(self, new_gripper_pos):
+        """Set the target gripper position."""
+        self._gripper_pos = np.clip(new_gripper_pos,
+                                    self.env.action_space.low[self.gripper_action_idx],
+                                    self.env.action_space.high[self.gripper_action_idx])
+
+class RecordStatus(Enum):
+    """Status for recording."""
+    INITIAL = 0
+    PRE_REACH = 1
+    REACH = 2
+    GRASP = 3
+    TELEOP = 4
+    END = 5
+
+class RecordKey(Enum):
+    """Data key for recording."""
+    TIME = 0
+    JOINT = 1
+    FRONT_IMAGE = 2
+    SIDE_IMAGE = 3
+    WRENCH = 4
+
+    def key(self):
+        """Get the key of the dictionary."""
+        return self.name.lower()
+
+class RecordManager(object):
+    """Recording manager for demonstrations by teleoperation."""
+
+    def __init__(self, env):
+        self.env = env
+
+        self.data_idx = 0
+        self.world_idx = 0
+
+        self._original_pole_pos = self.env.unwrapped.model.body("poles").pos.copy()
+
+        self.reset()
+
+    def reset(self):
+        """Reset recording."""
+        self.status = RecordStatus(0)
+
+        self.data_seq = {}
+        for record_key in RecordKey:
+            self.data_seq[record_key.key()] = []
+
+    def appendSingleData(self, record_key, data):
+        """Append a single data to the data sequence."""
+        self.data_seq[record_key.key()].append(data)
+
+    def saveData(self, filename):
+        """Save data."""
+        os.makedirs(os.path.dirname(filename), exist_ok=True)
+        np.savez(filename,
+                 times=self.data_seq[RecordKey.TIME.key()],
+                 joints=self.data_seq[RecordKey.JOINT.key()],
+                 front_images=self.data_seq[RecordKey.FRONT_IMAGE.key()],
+                 side_images=self.data_seq[RecordKey.SIDE_IMAGE.key()],
+                 wrenches=self.data_seq[RecordKey.WRENCH.key()])
+        self.data_idx += 1
+
+    def goToNextStatus(self):
+        """Go to the next status."""
+        self.status = RecordStatus((self.status.value + 1) % len(RecordStatus))
+
+    def getStatusImage(self):
+        """Get the image corresponding to the current status."""
+        status_image = np.zeros((50, 224, 3), dtype=np.uint8)
+        if self.status == RecordStatus.INITIAL:
+            status_image[:, :] = np.array([200, 255, 200])
+        elif self.status == RecordStatus.PRE_REACH or self.status == RecordStatus.REACH or self.status == RecordStatus.GRASP:
+            status_image[:, :] = np.array([255, 255, 200])
+        elif self.status == RecordStatus.TELEOP:
+            status_image[:, :] = np.array([255, 200, 200])
+        elif self.status == RecordStatus.END:
+            status_image[:, :] = np.array([200, 200, 255])
+        else:
+            raise ValueError("Unknown status: {}".format(self.status))
+        cv2.putText(status_image, self.status.name, (5, 35), cv2.FONT_HERSHEY_DUPLEX, 1.0, (0, 0, 0), 2)
+        return status_image
+
+    def setupSimWorld(self, pole_pos_idx=None):
+        """Setup the simulation world."""
+        # Set position of poles
+        pole_pos_offsets = np.array([
+            [-0.03, 0, 0.0],
+            [0.0, 0, 0.0],
+            [0.03, 0, 0.0],
+            [0.06, 0, 0.0],
+            [0.09, 0, 0.0],
+            [0.12, 0, 0.0],
+        ]) # [m]
+        if pole_pos_idx is None:
+            pole_pos_idx = self.data_idx % len(pole_pos_offsets)
+        self.env.unwrapped.model.body("poles").pos = self._original_pole_pos + pole_pos_offsets[pole_pos_idx]
+
+        # Set world index (currently only the position of poles is a dependent parameter)
+        self.world_idx = pole_pos_idx
+
+    @property
+    def status_elapsed_duration(self):
+        """Get the elapsed duration of the current status."""
+        return self.env.unwrapped.data.time - self.status_start_time
+
+    @property
+    def status(self):
+        """Get the status."""
+        return self._status
+
+    @status.setter
+    def status(self, new_status):
+        """Set the status."""
+        self._status = new_status
+        self.status_start_time = self.env.unwrapped.data.time
