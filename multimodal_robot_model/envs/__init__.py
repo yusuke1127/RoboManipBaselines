a060f5f2df8af8cc389a1384473fec8e70d589f9
Masaki Murooka <m-murooka@aist.go.jp>
2024-09-05 11:34:04 +0900

    Add task to roll up cloth in mujoco.

--- multimodal_robot_model/envs/__init__.py
+++ multimodal_robot_model/envs/__init__.py
@@ -14,3 +14,7 @@ register(
     id="multimodal_robot_model/UR5eScoopEnv-v0",
     entry_point="multimodal_robot_model.envs.mujoco:UR5eScoopEnv",
 )
+register(
+    id="multimodal_robot_model/UR5eClothEnv-v0",
+    entry_point="multimodal_robot_model.envs.mujoco:UR5eClothEnv",
+)
--- multimodal_robot_model/envs/mujoco/__init__.py
+++ multimodal_robot_model/envs/mujoco/__init__.py
@@ -1,3 +1,4 @@
 from .UR5eCableEnv import UR5eCableEnv
 from .UR5eRingEnv import UR5eRingEnv
 from .UR5eScoopEnv import UR5eScoopEnv
+from .UR5eClothEnv import UR5eClothEnv
