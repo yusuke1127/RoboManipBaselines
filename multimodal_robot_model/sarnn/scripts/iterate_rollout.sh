7b1f027e1fce852d899eee046ffc7d82b7a83589
Masaki Murooka <m-murooka@aist.go.jp>
2024-09-12 10:33:17 +0900

    [SARNN] Add crop option.

--- multimodal_robot_model/sarnn/scripts/iterate_rollout.sh
+++ multimodal_robot_model/sarnn/scripts/iterate_rollout.sh
@@ -1,15 +1,17 @@
 #!/bin/bash
 
-[[ $# < 1 ]] && echo "$0 <CKPT_DIR> [<CKPT_NAME> <TASK_NAME> <SKIP>]" && exit 1
+[[ $# < 1 ]] && echo "$0 <CKPT_DIR> [<CKPT_NAME> <TASK_NAME> <CROPPED_IMG_SIZE> <SKIP>]" && exit 1
 
 CKPT_DIR=$1
 CKPT_NAME=${2:-SARNN.pth}
 TASK_NAME=${3:-UR5eCable}
-SKIP=${4:-6}
+CROPPED_IMG_SIZE=${4:-280}
+SKIP=${5:-6}
 
 echo "[sarnn/iterate_rollout.sh] CKPT_DIR: ${CKPT_DIR}"
 echo "[sarnn/iterate_rollout.sh] CKPT_NAME: ${CKPT_NAME}"
 echo "[sarnn/iterate_rollout.sh] TASK_NAME: ${TASK_NAME}"
+echo "[sarnn/iterate_rollout.sh] CROPPED_IMG_SIZE: ${CROPPED_IMG_SIZE}"
 echo "[sarnn/iterate_rollout.sh] SKIP: ${SKIP}"
 
 SCRIPT_DIR=$(cd $(dirname $0); pwd)
@@ -20,6 +22,7 @@ for WORLD_IDX in "${WORLD_IDX_LIST[@]}"; do
     echo "[sarnn/iterate_rollout.sh] WORLD_IDX: ${WORLD_IDX}"
     python ${SCRIPT_DIR}/../bin/RolloutSarnn${TASK_NAME}.py \
 --checkpoint ${CKPT_DIR}/${CKPT_NAME} \
+--cropped_img_size ${CROPPED_IMG_SIZE} \
 --skip ${SKIP} \
 --world_idx ${WORLD_IDX} \
 --win_xy_policy 0 700 ${FIRST_OPTION}
