1073e3cca59c595a7ace2950c2fe4fe18c009dc6
Masaki Murooka <m-murooka@aist.go.jp>
2024-10-07 19:39:20 +0900

    Add version file.

--- setup.py
+++ setup.py
@@ -1,8 +1,15 @@
 from setuptools import setup
+from distutils.util import convert_path
+
+# https://stackoverflow.com/a/24517154
+main_ns = {}
+version_path = convert_path("multimodal_robot_model/version.py")
+with open(version_path) as version_file:
+    exec(version_file.read(), main_ns)
 
 setup(
     name="multimodal_robot_model",
-    version="0.0.1",
+    version=main_ns["__version__"],
     install_requires=[
         "matplotlib>=3.3.4",
         "gymnasium==0.29.1",
