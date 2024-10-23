8697f7d9b2387a45280a8202bac6d40a37233a41
Masaki Murooka <m-murooka@aist.go.jp>
2024-10-23 17:24:21 +0900

    [envs] Update MJCF files to update MuJoCo 3.0

--- multimodal_robot_model/common/rollout/RolloutMujocoUR5eCloth.py
+++ multimodal_robot_model/common/rollout/RolloutMujocoUR5eCloth.py
@@ -15,11 +15,11 @@ class RolloutMujocoUR5eCloth(RolloutBase):
     def setArmCommand(self):
         if self.data_manager.status in (MotionStatus.PRE_REACH, MotionStatus.REACH):
             target_se3 = pin.SE3(pin.rpy.rpyToMatrix(np.pi/2, 0.0, 0.25*np.pi),
-                                 self.env.unwrapped.get_body_pose("cloth_root_frame")[0:3])
+                                 self.env.unwrapped.get_body_pose("board")[0:3])
             if self.data_manager.status == MotionStatus.PRE_REACH:
-                target_se3 *= pin.SE3(np.identity(3), np.array([0.0, -0.2, -0.25]))
+                target_se3 *= pin.SE3(pin.rpy.rpyToMatrix(0.0, 0.125*np.pi, 0.0), np.array([0.0, -0.2, -0.4]))
             elif self.data_manager.status == MotionStatus.REACH:
-                target_se3 *= pin.SE3(np.identity(3), np.array([0.0, -0.2, -0.2]))
+                target_se3 *= pin.SE3(np.identity(3), np.array([0.0, -0.2, -0.3]))
             self.motion_manager.target_se3 = target_se3
             self.motion_manager.inverseKinematics()
         else:
