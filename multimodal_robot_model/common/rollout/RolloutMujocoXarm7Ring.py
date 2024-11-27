7a105fcaaa62a75df9b50f67d94d594e402366f1
Masaki Murooka <m-murooka@aist.go.jp>
2024-11-27 23:43:28 +0900

    [common] Add rollout classes for Xarm7 and ALOHA.

---
+++ multimodal_robot_model/common/rollout/RolloutMujocoXarm7Ring.py
@@ -0,0 +1,26 @@
+import numpy as np
+import pinocchio as pin
+import gymnasium as gym
+import multimodal_robot_model
+from multimodal_robot_model.common import MotionStatus
+from .RolloutBase import RolloutBase
+
+class RolloutMujocoXarm7Ring(RolloutBase):
+    def setupEnv(self):
+        self.env = gym.make(
+            "multimodal_robot_model/MujocoXarm7RingEnv-v0",
+            render_mode="human"
+        )
+
+    def setArmCommand(self):
+        if self.data_manager.status in (MotionStatus.PRE_REACH, MotionStatus.REACH):
+            target_pos = 0.5 * (self.env.unwrapped.get_geom_pose("fook1")[0:3] +
+                                self.env.unwrapped.get_geom_pose("fook2")[0:3])
+            if self.data_manager.status == MotionStatus.PRE_REACH:
+                target_pos += np.array([-0.2, 0.05, -0.05]) # [m]
+            elif self.data_manager.status == MotionStatus.REACH:
+                target_pos += np.array([-0.15, 0.05, -0.05]) # [m]
+            self.motion_manager.target_se3 = pin.SE3(pin.rpy.rpyToMatrix(0.0, 1.5 * np.pi, np.pi), target_pos)
+            self.motion_manager.inverseKinematics()
+        else:
+            super().setArmCommand()
