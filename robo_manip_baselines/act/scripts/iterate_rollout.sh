084c4127b89203504458dfb8f5c8f1e47103b436
yu-kitagawa <yu-kitagawa@aist.go.jp>
2024-12-06 17:27:28 +0900

    fixed iterate_rollout

--- robo_manip_baselines/act/scripts/iterate_rollout.sh
+++ robo_manip_baselines/act/scripts/iterate_rollout.sh
@@ -3,12 +3,10 @@
 [[ $# < 1 ]] && echo "$0 <CKPT_DIR> [<CKPT_NAME> <TASK_NAME> <SKIP>]" && exit 1
 
 CKPT_DIR=$1
-CKPT_NAME=${2:-policy_last.ckpt}
-TASK_NAME=${3:-MujocoUR5eCable}
-SKIP=${4:-3}
+TASK_NAME=${2:-MujocoUR5eCable}
+SKIP=${3:-3}
 
 echo "[act/iterate_rollout.sh] CKPT_DIR: ${CKPT_DIR}"
-echo "[act/iterate_rollout.sh] CKPT_NAME: ${CKPT_NAME}"
 echo "[act/iterate_rollout.sh] TASK_NAME: ${TASK_NAME}"
 echo "[act/iterate_rollout.sh] SKIP: ${SKIP}"
 
@@ -19,8 +17,7 @@ WORLD_IDX_LIST=(0 1 2 3 4 5)
 for WORLD_IDX in "${WORLD_IDX_LIST[@]}"; do
     echo "[act/iterate_rollout.sh] WORLD_IDX: ${WORLD_IDX}"
     python ${SCRIPT_DIR}/../bin/rollout/RolloutAct${TASK_NAME}.py \
-           --ckpt_dir ${CKPT_DIR} --ckpt_name ${CKPT_NAME} \
-           --chunk_size 100 --seed 42 \
+           --checkpoint ${CKPT_DIR} \
            --skip ${SKIP} \
            --world_idx ${WORLD_IDX} \
            --win_xy_policy 0 700 ${FIRST_OPTION}
--- robo_manip_baselines/sarnn/scripts/iterate_rollout.sh
+++ robo_manip_baselines/sarnn/scripts/iterate_rollout.sh
@@ -3,13 +3,11 @@
 [[ $# < 1 ]] && echo "$0 <CKPT_DIR> [<CKPT_NAME> <TASK_NAME> <CROPPED_IMG_SIZE> <SKIP>]" && exit 1
 
 CKPT_DIR=$1
-CKPT_NAME=${2:-SARNN.pth}
-TASK_NAME=${3:-MujocoUR5eCable}
-CROPPED_IMG_SIZE=${4:-280}
-SKIP=${5:-6}
+TASK_NAME=${2:-MujocoUR5eCable}
+CROPPED_IMG_SIZE=${3:-280}
+SKIP=${4:-6}
 
 echo "[sarnn/iterate_rollout.sh] CKPT_DIR: ${CKPT_DIR}"
-echo "[sarnn/iterate_rollout.sh] CKPT_NAME: ${CKPT_NAME}"
 echo "[sarnn/iterate_rollout.sh] TASK_NAME: ${TASK_NAME}"
 echo "[sarnn/iterate_rollout.sh] CROPPED_IMG_SIZE: ${CROPPED_IMG_SIZE}"
 echo "[sarnn/iterate_rollout.sh] SKIP: ${SKIP}"
@@ -21,7 +19,7 @@ WORLD_IDX_LIST=(0 1 2 3 4 5)
 for WORLD_IDX in "${WORLD_IDX_LIST[@]}"; do
     echo "[sarnn/iterate_rollout.sh] WORLD_IDX: ${WORLD_IDX}"
     python ${SCRIPT_DIR}/../bin/rollout/RolloutSarnn${TASK_NAME}.py \
-           --checkpoint ${CKPT_DIR}/${CKPT_NAME} \
+           --checkpoint ${CKPT_DIR} \
            --cropped_img_size ${CROPPED_IMG_SIZE} \
            --skip ${SKIP} \
            --world_idx ${WORLD_IDX} \
