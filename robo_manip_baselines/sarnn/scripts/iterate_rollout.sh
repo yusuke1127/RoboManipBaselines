0eab92b3b77825d3961ff9e0b22fcd19975fe136
Masaki Murooka <m-murooka@aist.go.jp>
2024-12-07 21:50:22 +0900

    Update iterate_rollout.sh

--- robo_manip_baselines/sarnn/scripts/iterate_rollout.sh
+++ robo_manip_baselines/sarnn/scripts/iterate_rollout.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 
-[[ $# < 1 ]] && echo "$0 <CKPT_DIR> [<CKPT_NAME> <TASK_NAME> <CROPPED_IMG_SIZE> <SKIP>]" && exit 1
+[[ $# < 1 ]] && echo "$0 <CHECKPOINT> <TASK_NAME> [<CROPPED_IMG_SIZE> <SKIP>]" && exit 1
 
-CKPT_DIR=$1
-TASK_NAME=${2:-MujocoUR5eCable}
+CHECKPOINT=$1
+TASK_NAME=$2
 CROPPED_IMG_SIZE=${3:-280}
 SKIP=${4:-6}
 
-echo "[sarnn/iterate_rollout.sh] CKPT_DIR: ${CKPT_DIR}"
+echo "[sarnn/iterate_rollout.sh] CHECKPOINT: ${CHECKPOINT}"
 echo "[sarnn/iterate_rollout.sh] TASK_NAME: ${TASK_NAME}"
 echo "[sarnn/iterate_rollout.sh] CROPPED_IMG_SIZE: ${CROPPED_IMG_SIZE}"
 echo "[sarnn/iterate_rollout.sh] SKIP: ${SKIP}"
@@ -19,7 +19,7 @@ WORLD_IDX_LIST=(0 1 2 3 4 5)
 for WORLD_IDX in "${WORLD_IDX_LIST[@]}"; do
     echo "[sarnn/iterate_rollout.sh] WORLD_IDX: ${WORLD_IDX}"
     python ${SCRIPT_DIR}/../bin/rollout/RolloutSarnn${TASK_NAME}.py \
-           --checkpoint ${CKPT_DIR} \
+           --checkpoint ${CHECKPOINT} \
            --cropped_img_size ${CROPPED_IMG_SIZE} \
            --skip ${SKIP} \
            --world_idx ${WORLD_IDX} \
