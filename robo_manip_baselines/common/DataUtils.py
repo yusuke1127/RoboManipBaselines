e12037cd7b2337b7d8414ca73d39d5a00293d1bf
Masaki Murooka <m-murooka@aist.go.jp>
2025-01-15 18:48:23 +0900

    Add DataUtils.py

---
+++ robo_manip_baselines/common/DataUtils.py
@@ -0,0 +1,65 @@
+from functools import reduce
+
+import numpy as np
+import pinocchio as pin
+
+
+def aggregate_data_seq_with_skip(data_seq, skip, agg_func):
+    """
+    Aggregate elements along the first axis (axis=0) in chunks of skip.
+
+    Parameters:
+        data_seq (np.ndarray): Input data sequence.
+        skip (int): Group size.
+        agg_func (callable): A function that takes a group (shape: (skip, ...)) as input and returns the aggregated result.
+
+    Returns:
+        np.ndarray: New data sequence where the specified function has been applied to grouped elements along the first axis.
+    """
+    # Add zero padding to the first axis
+    size = data_seq.shape[0]
+    padding = (skip - size % skip) % skip
+    if padding > 0:
+        pad_shape = [(0, padding)] + [(0, 0)] * (data_seq.ndim - 1)
+        data_seq = np.pad(
+            data_seq, pad_width=pad_shape, mode="constant", constant_values=0
+        )
+
+    # Reshape into skip groups along the first axis
+    grouped_data_seq = data_seq.reshape(-1, skip, *data_seq.shape[1:])
+
+    # Aggregate array by applying the user-defined function to each group
+    aggregated_data_seq = np.array([agg_func(arr) for arr in grouped_data_seq])
+
+    return aggregated_data_seq
+
+
+def get_skipped_data_seq(data_seq, key, skip):
+    """Get skipped data sequence."""
+    if key in (
+        DataKey.MEASURED_JOINT_POS_REL,
+        DataKey.COMMAND_JOINT_POS_REL,
+    ):
+
+        def sum_regular(arr):
+            return np.sum(arr, axis=0)
+
+        skipped_data_seq = aggregate_data_seq_with_skip(data_seq, skip, sum_regular)
+    elif key in (
+        DataKey.MEASURED_EEF_POSE_REL,
+        DataKey.COMMAND_EEF_POSE_REL,
+    ):
+
+        def sum_rpy(arr):
+            def add_rpy(rpy1, rpy2):
+                return pin.rpy.matrixToRpy(
+                    pin.rpy.rpyToMatrix(rpy1) @ pin.rpy.rpyToMatrix(rpy2)
+                )
+
+            return reduce(add_rpy, arr)
+
+        skipped_data_seq = aggregate_data_seq_with_skip(data_seq, skip, sum_rpy)
+    else:
+        skipped_data_seq = data_seq[::skip]
+
+    return skipped_data_seq
--- robo_manip_baselines/common/__init__.py
+++ robo_manip_baselines/common/__init__.py
@@ -2,6 +2,7 @@ from .MotionManager import MotionManager
 from .DataKey import DataKey
 from .DataManager import DataManager
 from .DataManagerVec import DataManagerVec
+from .DataUtils import get_skipped_data_seq
 from .Phase import Phase, PhaseOrder
 from .PhaseManager import PhaseManager
 from .VisionUtils import (
