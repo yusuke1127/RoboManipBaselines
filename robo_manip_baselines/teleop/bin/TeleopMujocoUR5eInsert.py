94b23ae3df86713839f7075fa513bc46a38d6099
Masaki Murooka <m-murooka@aist.go.jp>
2025-01-19 22:30:45 +0900

    Change MujocoUR5eInsertEnv to fix gripper and peg.

--- robo_manip_baselines/teleop/bin/TeleopMujocoUR5eInsert.py
+++ robo_manip_baselines/teleop/bin/TeleopMujocoUR5eInsert.py
@@ -13,17 +13,13 @@ class TeleopMujocoUR5eInsert(TeleopBase):
         )
         self.demo_name = self.args.demo_name or "MujocoUR5eInsert"
 
-    def set_arm_command(self):
-        if self.phase_manager.phase in (Phase.PRE_REACH, Phase.REACH):
-            target_pos = self.env.unwrapped.get_body_pose("peg")[0:3]
-            if self.phase_manager.phase == Phase.PRE_REACH:
-                target_pos[2] = 1.1  # [m]
-            elif self.phase_manager.phase == Phase.REACH:
-                target_pos[2] = 1.03  # [m]
-            target_se3 = pin.SE3(np.diag([-1.0, 1.0, -1.0]), target_pos)
-            self.motion_manager.set_command_data(DataKey.COMMAND_EEF_POSE, target_se3)
+    def set_gripper_command(self):
+        if self.phase_manager.phase == Phase.GRASP:
+            self.motion_manager.set_command_data(
+                DataKey.COMMAND_GRIPPER_JOINT_POS, np.array([170.0])
+            )
         else:
-            super().set_arm_command()
+            super().set_gripper_command()
 
 
 if __name__ == "__main__":
