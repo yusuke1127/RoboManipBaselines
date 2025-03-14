fc9bb766c1e38b4d5e291c9e8d8981b8b0118730
ysk-kimura <kimura.ysk@aist.go.jp>
2025-03-14 17:47:39 +0900

    [utils] Define DisplayCameraImage.py.

--- robo_manip_baselines/utils/DisplayCameraImage.py
+++ robo_manip_baselines/utils/DisplayCameraImage.py
@@ -1,20 +1,22 @@
 import cv2
 
-cap = cv2.VideoCapture(12)
 
-cv2.namedWindow("Webcam Live", cv2.WINDOW_NORMAL)
-cv2.resizeWindow("Webcam Live", 810, 490)
-cv2.moveWindow("Webcam Live", 0, 0)
-while cap.isOpened():
-    ret, frame = cap.read()
-    if ret:
-        resize_frame = cv2.resize(frame, (810, 490))
-        cv2.imshow("Webcam Live", resize_frame)
+class DisplayCameraImage:
+    cap = cv2.VideoCapture(12)
 
-        if cv2.waitKey(1) & 0xFF == ord("n"):
+    cv2.namedWindow("Webcam Live", cv2.WINDOW_NORMAL)
+    cv2.resizeWindow("Webcam Live", 810, 490)
+    cv2.moveWindow("Webcam Live", 0, 0)
+    while cap.isOpened():
+        ret, frame = cap.read()
+        if ret:
+            resize_frame = cv2.resize(frame, (810, 490))
+            cv2.imshow("Webcam Live", resize_frame)
+
+            if cv2.waitKey(1) & 0xFF == ord("n"):
+                break
+        else:
             break
-    else:
-        break
 
-cap.release()
-cv2.destroyAllWindows()
+    cap.release()
+    cv2.destroyAllWindows()
