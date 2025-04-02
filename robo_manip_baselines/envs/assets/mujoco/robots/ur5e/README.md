a043806408f3b2cc5c8d9e7f1ec5e3daa99fe55c
Masaki Murooka <m-murooka@aist.go.jp>
2025-04-02 16:34:16 +0900

    [ur5e] Automatically generate left and right versions of the model.

---
+++ robo_manip_baselines/envs/assets/mujoco/robots/ur5e/README.md
@@ -0,0 +1,2 @@
+> [!NOTE]
+> Some xml files in this directory are automatically generated from *.in.xml files by generate.py. If you need to modify the automatically generated files, edit the original *.in.xml file and run generate.py.
---
+++ robo_manip_baselines/envs/assets/mujoco/robots/ur5e/generate.py
@@ -0,0 +1,24 @@
+#! /usr/bin/env python
+
+
+def generate(template_file, prefix_list):
+    with open(template_file, "r", encoding="utf-8") as f:
+        template_text = f.read()
+
+    for prefix in prefix_list:
+        if prefix == "":
+            output_file = template_file.replace(".in", "")
+        else:
+            output_file = template_file.replace(".in", "_" + prefix.rstrip("/"))
+
+        output_text = template_text.format(PREFIX=prefix)
+
+        with open(output_file, "w", encoding="utf-8") as f:
+            f.write(output_text)
+
+        print(f"Generated: {output_file}")
+
+
+prefix_list = ["", "left/", "right/"]
+generate("ur5e_integrated_config.in.xml", prefix_list)
+generate("ur5e_integrated_body.in.xml", prefix_list)
