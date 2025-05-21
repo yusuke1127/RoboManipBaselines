0bf6a01d6261138fef3f24d871543ca16f54b134
Masaki Murooka <m-murooka@aist.go.jp>
2025-05-21 15:08:24 +0900

    [envs] Implement get_success in MujocoUR5eToolboxEnv

--- robo_manip_baselines/envs/mujoco/ur5e/MujocoUR5eToolboxEnv.py
+++ robo_manip_baselines/envs/mujoco/ur5e/MujocoUR5eToolboxEnv.py
@@ -1,5 +1,6 @@
 from os import path
 
+import mujoco
 import numpy as np
 
 from .MujocoUR5eEnvBase import MujocoUR5eEnvBase
@@ -41,6 +42,20 @@ class MujocoUR5eToolboxEnv(MujocoUR5eEnvBase):
         #         [0.0, 0.09, 0.0],
         #     ]
         # )  # [m]
+
+    def _get_success(self):
+        toolbox_body_id = mujoco.mj_name2id(
+            self.model, mujoco.mjtObj.mjOBJ_BODY, "toolbox"
+        )
+        toolbox_pos = self.data.xpos[toolbox_body_id].copy()
+        mat_body_id = mujoco.mj_name2id(self.model, mujoco.mjtObj.mjOBJ_BODY, "mat")
+        mat_pos = self.data.xpos[mat_body_id].copy()
+
+        xy_thre = 0.03  # [m]
+        z_thre = mat_pos[2] + 0.005  # [m]
+        return (np.max(np.abs(toolbox_pos[:2] - mat_pos[:2])) < xy_thre) and (
+            toolbox_pos[2] < z_thre
+        )
 
     def modify_world(self, world_idx=None, cumulative_idx=None):
         # if world_idx is None:
