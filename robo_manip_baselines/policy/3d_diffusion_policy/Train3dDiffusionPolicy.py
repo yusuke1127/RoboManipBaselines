2331bc38d7d10b27f851deb4c36901e69b24fe19
Naoki Shibayama <naoki.shibayama@aist.go.jp>
2025-05-29 16:15:03 +0900

    Code Dataset, Train section and update pyproject.
    Update pyproject.toml based on third_party/3D-Diffusion-Policy/INSTALL.md
    Change file and class name: DiffusionPolicy3DDataset(.py)
    Code base of DiffusionPolicy3DDataset.py, Train3dDiffusionPolicy.py (based on DiffusionPolicyDataset.py, TrainDiffusionPolicy.py)

---
+++ robo_manip_baselines/policy/3d_diffusion_policy/Train3dDiffusionPolicy.py
@@ -0,0 +1,232 @@
+import argparse
+import copy
+
+import torch
+from diffusers.schedulers.scheduling_ddpm import DDPMScheduler
+from diffusion_policy_3d.common.pytorch_util import dict_apply, optimizer_to
+from diffusion_policy_3d.model.common.lr_scheduler import get_scheduler
+from diffusion_policy_3d.model.diffusion.ema_model import EMAModel
+from diffusion_policy_3d.policy.dp3 import DP3
+from tqdm import tqdm
+
+from robo_manip_baselines.common import DataKey, TrainBase
+
+from .DiffusionPolicy3DDataset import DiffusionPolicy3DDataset
+
+
+class Train3dDiffusionPolicy(TrainBase):
+    DatasetClass = DiffusionPolicy3DDataset
+
+    def setup_args(self):
+        super().setup_args()
+
+    def set_additional_args(self, parser):
+        parser.set_defaults(enable_rmb_cache=True)
+
+        parser.set_defaults(norm_type="limits")
+
+        parser.set_defaults(batch_size=64)
+        parser.set_defaults(num_epochs=2000)
+        parser.set_defaults(lr=1e-4)
+
+        parser.add_argument(
+            "--weight_decay", type=float, default=1e-6, help="weight decay"
+        )
+
+        parser.add_argument(
+            "--use_ema",
+            action=argparse.BooleanOptionalAction,
+            default=True,
+            help="Enable or disable exponential moving average (EMA)",
+        )
+
+        parser.add_argument(
+            "--horizon", type=int, default=16, help="prediction horizon"
+        )
+        parser.add_argument(
+            "--n_obs_steps",
+            type=int,
+            default=2,
+            help="number of steps in observation to input in the policy",
+        )
+        parser.add_argument(
+            "--n_action_steps",
+            type=int,
+            default=8,
+            help="number of steps in the action to output from the policy",
+        )
+
+        parser.add_argument(
+            "--image_size",
+            type=int,
+            nargs=2,
+            default=[320, 240],
+            help="Image size (width, height) to be resized before crop. In the case of multiple image inputs, it is assumed that all images share the same size.",
+        )
+        parser.add_argument(
+            "--image_crop_size",
+            type=int,
+            nargs=2,
+            default=[288, 216],
+            help="Image size (width, height) to be cropped after resize. In the case of multiple image inputs, it is assumed that all images share the same size.",
+        )
+
+    def setup_model_meta_info(self):
+        super().setup_model_meta_info()
+
+        self.model_meta_info["data"]["image_size"] = self.args.image_size
+        self.model_meta_info["data"]["image_crop_size"] = self.args.image_crop_size
+        self.model_meta_info["data"]["horizon"] = self.args.horizon
+        self.model_meta_info["data"]["n_obs_steps"] = self.args.n_obs_steps
+        self.model_meta_info["data"]["n_action_steps"] = self.args.n_action_steps
+
+        self.model_meta_info["policy"]["use_ema"] = self.args.use_ema
+
+    def get_extra_norm_config(self):
+        if self.args.norm_type == "limits":
+            return {
+                "out_min": -1.0,
+                "out_max": 1.0,
+            }
+        else:
+            return super().get_extra_norm_config()
+
+    def setup_policy(self):
+        # Set policy args
+        shape_meta = {
+            "obs": {},
+            "action": {"shape": [len(self.model_meta_info["action"]["example"])]},
+        }
+        if len(self.args.state_keys) > 0:
+            shape_meta["obs"]["state"] = {
+                "shape": [len(self.model_meta_info["state"]["example"])],
+                "type": "low_dim",
+            }
+        for camera_name in self.args.camera_names:
+            shape_meta["obs"][DataKey.get_rgb_image_key(camera_name)] = {
+                "shape": [3, self.args.image_size[1], self.args.image_size[0]],
+                "type": "rgb",
+            }
+        self.model_meta_info["policy"]["args"] = {
+            "shape_meta": shape_meta,
+            "horizon": self.args.horizon,
+            "n_action_steps": self.args.n_action_steps,
+            "n_obs_steps": self.args.n_obs_steps,
+            "num_inference_steps": 100,
+            "obs_as_global_cond": True,
+            "crop_shape": self.args.image_crop_size[::-1],  # (height, width)
+            "diffusion_step_embed_dim": 128,
+            "down_dims": [512, 1024, 2048],
+            "kernel_size": 5,
+            "n_groups": 8,
+            "cond_predict_scale": True,
+            "obs_encoder_group_norm": True,
+            "eval_fixed_crop": True,
+        }
+        self.model_meta_info["policy"]["noise_scheduler_args"] = {
+            "beta_end": 0.02,
+            "beta_schedule": "squaredcos_cap_v2",
+            "beta_start": 0.0001,
+            "clip_sample": True,
+            "num_train_timesteps": 100,
+            "prediction_type": "epsilon",
+            "variance_type": "fixed_small",
+        }
+
+        # Construct policy
+        noise_scheduler = DDPMScheduler(
+            **self.model_meta_info["policy"]["noise_scheduler_args"]
+        )
+        self.policy = DP3(
+            noise_scheduler=noise_scheduler,
+            **self.model_meta_info["policy"]["args"],
+        )
+
+        # Construct exponential moving average (EMA)
+        if self.args.use_ema:
+            self.ema_policy = copy.deepcopy(self.policy)
+            self.ema = EMAModel(
+                model=self.ema_policy,
+                update_after_step=0,
+                inv_gamma=1.0,
+                power=0.75,
+                min_value=0.0,
+                max_value=0.9999,
+            )
+
+        # Construct optimizer
+        self.optimizer = torch.optim.AdamW(
+            self.policy.parameters(),
+            lr=self.args.lr,
+            weight_decay=self.args.weight_decay,
+            betas=(0.95, 0.999),
+            eps=1e-8,
+        )
+        self.lr_scheduler = get_scheduler(
+            name="cosine",
+            optimizer=self.optimizer,
+            num_warmup_steps=500,
+            num_training_steps=(len(self.train_dataloader) * self.args.num_epochs),
+        )
+
+        # Transfer to device
+        self.policy.cuda()
+        if self.args.use_ema:
+            self.ema_policy.cuda()
+        optimizer_to(self.optimizer, "cuda")
+
+        # Print policy information
+        self.print_policy_info()
+        print(f"  - use ema: {self.args.use_ema}")
+        print(
+            f"  - horizon: {self.args.horizon}, obs steps: {self.args.n_obs_steps}, action steps: {self.args.n_action_steps}"
+        )
+        print(
+            f"  - image size: {self.args.image_size}, image crop size: {self.args.image_crop_size}"
+        )
+
+    def train_loop(self):
+        for epoch in tqdm(range(self.args.num_epochs)):
+            # Run train step
+            batch_result_list = []
+            for data in self.train_dataloader:
+                loss, _ = self.policy.compute_loss(dict_apply(data, lambda x: x.cuda()))
+                loss.backward()
+                self.optimizer.step()
+                self.optimizer.zero_grad()
+                self.lr_scheduler.step()
+                if self.args.use_ema:
+                    self.ema.step(self.policy)
+                batch_result_list.append(
+                    self.detach_batch_result(
+                        {"loss": loss, "lr": self.lr_scheduler.get_last_lr()[0]}
+                    )
+                )
+            self.log_epoch_summary(batch_result_list, "train", epoch)
+
+            # Run validation step
+            if self.args.use_ema:
+                policy = self.ema_policy
+            else:
+                policy = self.policy
+            policy.eval()
+            with torch.inference_mode():
+                batch_result_list = []
+                for data in self.val_dataloader:
+                    loss, _ = policy.compute_loss(dict_apply(data, lambda x: x.cuda()))
+                    batch_result_list.append(self.detach_batch_result({"loss": loss}))
+                epoch_summary = self.log_epoch_summary(batch_result_list, "val", epoch)
+
+                # Update best checkpoint
+                self.update_best_ckpt(epoch_summary, policy=policy)
+            policy.train()
+
+            # Save current checkpoint
+            if epoch % max(self.args.num_epochs // 10, 1) == 0:
+                self.save_current_ckpt(f"epoch{epoch:0>4}", policy=policy)
+
+        # Save last checkpoint
+        self.save_current_ckpt("last", policy=policy)
+
+        # Save best checkpoint
+        self.save_best_ckpt()
