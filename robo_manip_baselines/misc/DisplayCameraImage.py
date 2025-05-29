0d077eeb7fb6f1fdb8a6a9e472f8ddc5c5c6d834
Masaki Murooka <m-murooka@aist.go.jp>
2025-05-29 16:19:45 +0900

    [misc] Update DisplayCameraImage.py

--- robo_manip_baselines/misc/DisplayCameraImage.py
+++ robo_manip_baselines/misc/DisplayCameraImage.py
@@ -3,66 +3,82 @@ import os
 import re
 
 import cv2
+import numpy as np
+
+from robo_manip_baselines.common import crop_and_resize
 
 
 class DisplayCameraImage:
-    def __init__(self, camera_device_name):
-        self.camera_device_name = camera_device_name
-        self.cap = self.find_device_capture(camera_device_name)
+    def __init__(self, camera_name=None, camera_id=None):
+        if (camera_name is not None) and (camera_id is None):
+            self.camera_name = camera_name
+            self.cap = self.get_capture_from_name(camera_name)
+        elif (camera_name is None) and (camera_id is not None):
+            self.cap = cv2.VideoCapture(camera_id)
+            self.camera_name = f"video{camera_id}"
+        else:
+            raise ValueError(
+                f"[{self.__class__.__name__}] Only one of camera_name and camera_id should be specified. camera_name: {camera_name}, camera_id: {camera_id}"
+            )
 
-    def find_device_capture(self, camera_device_name):
-        print(f"[{self.__class__.__name__}] {camera_device_name=}")
+    def get_capture_from_name(self, camera_name):
+        print(f"[{self.__class__.__name__}] {camera_name=}")
         cap = None
         for device_name in os.listdir("/sys/class/video4linux"):
             print(
-                f"[{self.__class__.__name__}] {self.find_device_capture.__name__}, {device_name=}"
+                f"[{self.__class__.__name__}] {self.get_capture_from_name.__name__}, {device_name=}"
             )
             real_device_name = os.path.realpath(
                 "/sys/class/video4linux/" + device_name + "/name"
             )
             with open(real_device_name, "r", encoding="utf-8") as device_name_file:
                 detected_device_id = device_name_file.read().rstrip()
-            if camera_device_name in detected_device_id:
+            if camera_name in detected_device_id:
                 device_num = int(re.search(r"\d+$", device_name).group(0))
                 print(
-                    f"[{self.__class__.__name__}] {self.find_device_capture.__name__}, "
+                    f"[{self.__class__.__name__}] {self.get_capture_from_name.__name__}, "
                     f"Found device. ID: {detected_device_id}"
                 )
                 cap = cv2.VideoCapture(device_num)
                 if cap is None or not cap.isOpened():
                     print(
-                        f"[{self.__class__.__name__}] {self.find_device_capture.__name__}, "
+                        f"[{self.__class__.__name__}] {self.get_capture_from_name.__name__}, "
                         f"Unable to open video source ({device_num=})."
                     )
                     continue
                 break
         if cap is None:
-            raise LookupError()
+            raise LookupError(
+                f"[{self.__class__.__name__}] Error: No device matching '{camera_name}' was found."
+            )
         return cap
 
-    def display_camera_output(self, horizontal_size, x_position, y_position):
-        cv2.namedWindow(f"{self.camera_device_name} Live", cv2.WINDOW_NORMAL)
-        cv2.moveWindow(f"{self.camera_device_name} Live", x_position, y_position)
+    def display_camera_output(self, crop_size=None, resize_width=None, win_xy=None):
+        window_name = f"{self.camera_name} Live"
+        cv2.namedWindow(window_name, cv2.WINDOW_NORMAL)
+        if win_xy is not None:
+            cv2.moveWindow(window_name, *win_xy)
 
         print(f"[{self.__class__.__name__}] Press q on image to exit.")
         while self.cap.isOpened():
             ret, frame = self.cap.read()
-            if ret:
-                if horizontal_size is not None:
-                    aspect_ratio = frame.shape[1] / frame.shape[0]
-                    vertical_size = int(horizontal_size / aspect_ratio)
-                    frame = cv2.resize(frame, (horizontal_size, vertical_size))
-                    cv2.resizeWindow(
-                        f"{self.camera_device_name} Live",
-                        horizontal_size,
-                        vertical_size,
-                    )
+            if not ret:
+                print(f"[{self.__class__.__name__}] Failed to read the capture.")
+                break
 
-                cv2.imshow(f"{self.camera_device_name} Live", frame)
-
-                if cv2.waitKey(1) & 0xFF == ord("q"):
-                    break
+            if resize_width is None:
+                resize_size = None
             else:
+                aspect_ratio = frame.shape[1] / frame.shape[0]
+                resize_height = int(resize_width / aspect_ratio)
+                resize_size = (resize_width, resize_height)
+                cv2.resizeWindow(window_name, *resize_size)
+
+            frame = crop_and_resize(frame[np.newaxis], crop_size, resize_size)[0]
+
+            cv2.imshow(window_name, frame)
+
+            if cv2.waitKey(1) & 0xFF == ord("q"):
                 break
 
         self.cap.release()
@@ -71,26 +87,17 @@ class DisplayCameraImage:
 
 def parse_arguments():
     parser = argparse.ArgumentParser()
-    parser.add_argument("-c", "--camera_device_name", type=str, default="Webcam")
-    parser.add_argument("-w", "--horizontal_size", default=810)
-    parser.add_argument("-x", "--x_position", default=0)
-    parser.add_argument("-y", "--y_position", default=0)
+    parser.add_argument("--camera_name", type=str, default=None)
+    parser.add_argument("--camera_id", type=int, default=None)
+    parser.add_argument("--crop_size", type=int, nargs=2, default=None)
+    parser.add_argument("--resize_width", type=int, default=None)
+    parser.add_argument("--win_xy", type=int, nargs=2)
     return parser.parse_args()
 
 
-def main():
-    args = parse_arguments()
-    try:
-        disp = DisplayCameraImage(args.camera_device_name)
-        disp.display_camera_output(
-            args.horizontal_size, args.x_position, args.y_position
-        )
-    except LookupError:
-        print(
-            f"[{DisplayCameraImage.__class__.__name__}] Error: "
-            f"No device matching '{args.camera_device_name}' was found."
-        )
-
-
 if __name__ == "__main__":
-    main()
+    args = parse_arguments()
+    disp = DisplayCameraImage(camera_name=args.camera_name, camera_id=args.camera_id)
+    disp.display_camera_output(
+        crop_size=args.crop_size, resize_width=args.resize_width, win_xy=args.win_xy
+    )
